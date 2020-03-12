eac7d85c3be55fddc425fd385c24bd470047b08e
ewwong2 <ewwong2@dons.usfca.edu>
2020-03-11 18:55:06 -0700

    Partial progress

---
+++ js/timebar.js
@@ -0,0 +1,227 @@
+// setup svg
+const svg = d3.select('body').select('svg#timebar');
+
+// get current hard-coded size of svg
+const size = {
+  width: parseInt(svg.attr('width')),
+  height: parseInt(svg.attr('height'))
+};
+
+// set padding
+const pad = {top: 15, right: 15, bottom: 25, left: 50};
+
+// setup plot area
+const plot = svg.select('g#plot');
+
+// setup plot width and height
+const width = size.width - pad.left - pad.right;
+const height = size.height - pad.top - pad.bottom;
+
+const x = d3.scaleLinear()
+  // .domain([countMin, countMax])
+  .range([pad.left, size.width - pad.right])
+  .nice()
+
+const y = d3.scaleBand()
+  // .domain([...airlines.keys()])
+  .range([pad.top, size.height - pad.bottom])
+  .padding(0.08)
+
+const color = d3.scaleOrdinal()
+  .range(d3.schemeTableau10);
+
+
+d3.csv("data/timebar.csv", convert).then(draw);
+
+var formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en")
+
+function draw(data) {
+  // console.table(data);
+  console.log(data);
+  let series = dataWrangling(data);
+  console.log(series[0]);
+  x.domain([0, d3.max(series, d => d3.max(d, d => d[1]))]);
+  y.domain(series[0].map(d => d.data.Zipcode));
+
+  color.domain(series.map(d => d.key));
+
+  xAxis = g => g
+    .attr("transform", `translate(0,${size.height - pad.bottom})`)
+    .call(d3.axisBottom(x).ticks(width / 100, "s").tickSizeOuter(0))
+    .call(g => g.selectAll(".domain"))
+
+  yAxis = g => g
+    .attr("transform", `translate(${pad.left},0)`)
+    .call(d3.axisLeft(y).tickSizeOuter(0))
+    .call(g => g.selectAll(".domain"))
+
+  svg.select('g#x')
+    .call(xAxis);
+
+  svg.select('g#y')
+    .call(yAxis);
+
+  console.log(series);
+
+  plot.selectAll("g#bars")
+    .data(series)
+    .join("g")
+      .attr("fill", d => color(d.key))
+    .selectAll("rect")
+    .data(d => d)
+    .join("rect")
+      .attr("x", d => x(d[0]))
+      .attr("y", (d, i) => y(d.data.Zipcode))
+      .attr("width", d => x(d[1]) - x(d[0]))
+      .attr("height", y.bandwidth())
+    .append("title")
+      .text(d => `
+Zipcode: ${d.data.Zipcode}
+${d.key}: ${formatValue(d.data[d.key])} minutes
+Total: ${d.data.Total}
+Count: ${d.data.Count}`);
+
+  // Type will be used as color so we will not make an axis for it
+  // dimensions = d3.keys(data[0]).filter(d => d != "Type");
+  //
+  // for (dim in dimensions) {
+  //   let name = dimensions[dim];
+  //   y[name] = d3.scaleLinear()
+  //     .domain(d3.extent(data, d => d[name]))
+  //     .range([height, 0]);
+  // }
+  //
+  // x.domain(dimensions);
+  //
+  // color.domain(data.map(d => d['Type']));
+  //
+  // // since our g elements are already in the proper order, we can draw our svg
+  // // elements in any order we want
+  // drawLines(data);
+  // drawAxis(data);
+  // drawLegend(data);
+}
+
+function dataWrangling(data) {
+  let data2 = d3.nest()
+    .key(d => d['Zipcode of Incident'])
+    .rollup(function(v) {
+      let temp = {};
+      temp['Zipcode'] = v[0]['Zipcode of Incident'];
+      let total = 0;
+      let count = 0;
+      for (col of ['Received to Entry', 'Entry to Dispatch', 'Dispatch to Response', 'Response to On Scene']) {
+        temp['Avg. ' + col] = d3.sum(v, d => d[col + ' sum']) / d3.sum(v, d => d[col + ' count']);
+        total += temp['Avg. ' + col];
+        count += d3.sum(v, d => d[col + ' count']);
+      }
+      temp['Total'] = total;
+      temp['Count'] = count;
+      return temp;
+    })
+    .object(data);
+  data2 = Object.values(data2);
+  data2['columns'] = ['Zipcode'].concat(['Received to Entry', 'Entry to Dispatch', 'Dispatch to Response', 'Response to On Scene'].map(col => 'Avg. ' + col));
+  data2 = data2.sort((a, b) => b['Total'] - a['Total'])
+  let stack = d3.stack()
+    .keys(data2.columns.slice(1))(data2)
+    .map(d => (d.forEach(v => v.key = d.key), d));
+  return stack;
+}
+
+function convert(input) {
+  input['Call Date'] = new Date(input['Call Date ']);
+  delete input['Call Date ']
+  for (col of ['Received to Entry', 'Entry to Dispatch', 'Dispatch to Response', 'Response to On Scene']) {
+    input[col + ' sum'] = +input[col + ' sum'];
+    input[col + ' count'] = +input[col + ' count'];
+  }
+  input['Zipcode of Incident'] = +input['Zipcode of Incident '];
+  delete input['Zipcode of Incident '];
+  return input;
+}
+
+/**
+ * Draw the axes for the parallel coordinates
+ */
+function drawAxis(data) {
+  // // Draw the axes
+  // svg.select('g#y').selectAll('myAxis')
+  //   // For each dimension of the dataset I add a 'g' element:
+  //   .data(dimensions).enter()
+  //   .append('g')
+  //   .attr("class", "axis")
+  //   // translate element to its right position on the x axis
+  //   .attr('transform', d => `translate(${x(d)})`)
+  //   // And I build the axis with the call function
+  //   .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+  //   // Add axis title
+  //   .append('text')
+  //     .style('text-anchor', 'middle')
+  //     .attr('y', height)
+  //     .attr('dy', 15)
+  //     .text(d => d)
+  //     .style('fill', 'black');
+}
+
+function drawLegend(data) {
+  // let types = [... new Set(data.map(d => d['Type']))];
+  // let lConfig = {
+  //   'x': 780,
+  //   'y': 20,
+  //   'width': 175,
+  //   'height': 120,
+  //   'p': 10,
+  //   'marker': {
+  //     'width': 15,
+  //     'height': 3,
+  //     'size': 15
+  //   }
+  // };
+  // // Legend from: https://www.d3-graph-gallery.com/graph/custom_legend.html
+  // svg.select("g#legend").append("rect")
+  //   .attr("fill", "whitesmoke")
+  //   .attr("stroke-width", 1)
+  //   .attr("stroke", "gainsboro")
+  //   .attr("x", lConfig.x)
+  //   .attr("y", lConfig.y)
+  //   .attr("rx", 8)
+  //   .attr("ry", 8)
+  //   .attr("width", lConfig.width)
+  //   .attr("height", lConfig.height);
+  //
+  // svg.select("g#legend").append("text")
+  //   .attr("x", lConfig.x + lConfig.width/2)
+  //   .attr("y", lConfig.y + 2*lConfig.p)
+  //   .attr("text-anchor", "middle")
+  //   .text("College Type");
+  //
+  // svg.select("g#legend").selectAll("mydots")
+  //   .data(types)
+  //   .enter()
+  //   .append("rect")
+  //     .attr("class", d => `legend ${kebabCase(d)}-legend`)
+  //     .attr("x", lConfig.x + lConfig.p)
+  //     .attr("y", (d,i) => lConfig.y + 3.5*lConfig.p + i*(lConfig.marker.size+5)) // 100 is where the first dot appears. 25 is the distance between dots
+  //     .attr("width", lConfig.marker.width)
+  //     .attr("height", lConfig.marker.height)
+  //     .style("fill", d => color(d))
+  //     .style("alignment-baseline", "middle")
+  //     .on("mouseover", d => {highlight({Type: d})})
+  //     .on("mouseleave", d => {doNotHighlight({Type: d})});
+
+  // Add one dot in the legend for each name.
+  // svg.select("g#legend").selectAll("mylabels")
+  //   .data(types)
+  //   .enter()
+  //   .append("text")
+  //     .attr("class", d => `legend ${kebabCase(d)}-legend`)
+  //     .attr("x", lConfig.x + 2*lConfig.p + lConfig.marker.size*1.2)
+  //     .attr("y", (d,i) => lConfig.y + 3*lConfig.p  + i*(lConfig.marker.size+5) + (lConfig.marker.size/2)) // 100 is where the first dot appears. 25 is the distance between dots
+  //     .style("fill", d => color(d))
+  //     .text(d => d)
+  //     .attr("text-anchor", "left")
+  //     .style("alignment-baseline", "middle")
+  //     .on("mouseover", d => {highlight({Type: d})})
+  //     .on("mouseleave", d => {doNotHighlight({Type: d})});
+}
