ca9c0d9fe4d462fe21e614dbc4a98b2a2430de30
ewwong2 <ewwong2@dons.usfca.edu>
2020-03-16 01:31:41 -0700

    Partial writeup

--- js/timebar.js
+++ js/timebar.js
@@ -25,7 +25,43 @@ let timeBarChart = data => {
   // Check initial chart animation status
   let animated = true;
 
+  // Mapping for neighborhoods
+  const zipToNeighborhood = {
+    94123.0: 'Pacific Heights/Marina/Russian Hill/Presidio',
+    94110.0: 'Mission/Potrero Hill/Bernal Heights/Castro/Upper Market/Noe Valley/Glen Park',
+    94103.0: 'South of Market/Financial District/South Beach/Mission/Castro/Upper Market/Hayes Valley/Tenderloin/Mission Bay/Potrero Hill',
+    94124.0: 'Bayview Hunters Point/Bernal Heights/Potrero Hill/Portola/None',
+    94108.0: 'Nob Hill/Chinatown/Financial District/South Beach',
+    94158.0: 'Mission Bay/Potrero Hill/None',
+    94127.0: 'West of Twin Peaks/Twin Peaks/Oceanview/Merced/Ingleside',
+    94102.0: 'Tenderloin/Western Addition/South of Market/Hayes Valley/Nob Hill/Financial District/South Beach',
+    94132.0: 'Lakeshore/Oceanview/Merced/Ingleside/Sunset/Parkside/West of Twin Peaks',
+    94134.0: 'Portola/Visitacion Valley/Excelsior/McLaren Park/Bayview Hunters Point/None',
+    94112.0: 'West of Twin Peaks/Excelsior/Outer Mission/Oceanview/Merced/Ingleside/McLaren Park/Bernal Heights',
+    94109.0: 'Tenderloin/Nob Hill/Russian Hill/Pacific Heights/Western Addition/Japantown/Marina',
+    94117.0: 'Haight Ashbury/Hayes Valley/Lone Mountain/USF/Inner Sunset/Western Addition/Castro/Upper Market/Golden Gate Park',
+    94122.0: 'Sunset/Parkside/Inner Sunset/Golden Gate Park/Lone Mountain/USF/Inner Richmond/Outer Richmond',
+    'Unknown': 'None/Excelsior/Visitacion Valley',
+    94133.0: 'Chinatown/North Beach/Nob Hill/Russian Hill',
+    94131.0: 'Noe Valley/Glen Park/Inner Sunset/Twin Peaks/West of Twin Peaks/Castro/Upper Market',
+    94111.0: 'Chinatown/Financial District/South Beach/North Beach',
+    94118.0: 'Inner Richmond/Outer Richmond/Lone Mountain/USF/Presidio Heights/Golden Gate Park/Seacliff/Presidio',
+    94115.0: 'Pacific Heights/Western Addition/Japantown/Presidio Heights/Lone Mountain/USF/Presidio',
+    94129.0: 'Presidio/Inner Richmond/Seacliff',
+    94104.0: 'Financial District/South Beach/Chinatown',
+    94114.0: 'Castro/Upper Market/Noe Valley/Hayes Valley/Twin Peaks/Mission/Inner Sunset/Haight Ashbury',
+    94116.0: 'Sunset/Parkside/Inner Sunset/West of Twin Peaks',
+    94107.0: 'Potrero Hill/Mission Bay/South of Market/Financial District/South Beach/Bayview Hunters Point/Mission',
+    94105.0: 'Financial District/South Beach/South of Market',
+    94130.0: 'Treasure Island',
+    94121.0: 'Outer Richmond/Seacliff/Golden Gate Park/Lincoln Park'
+  };
+
   let draw = (data, animate) => {
+    let formatTime = d3.timeFormat("%B %d, %Y");
+    let daterange = d3.extent(data, d => new Date(d['Call Date']));
+    d3.select("text#daterange")
+      .text(`From ${formatTime(daterange[0])} to ${formatTime(daterange[1])}`);
     let series = dataWrangling(data);
 
     drawAxis(series);
@@ -207,6 +243,30 @@ let timeBarChart = data => {
       .on("mouseover", tooltip)
       .on("mousemove", tooltipMove)
       .on("mouseout", tooltipLeave);
+
+    svg.select('g#y').selectAll(".tick text")
+      .on("mouseover", d => {
+        let me = d3.select(this);
+        let div = d3.select("body").append("div");
+
+        div.attr("id", "details");
+        div.attr("class", "tooltip");
+
+        let rows = div.append("table")
+          .selectAll("tr")
+          .data(["Zipcode: ", "Neighborhoods: "])
+          .enter()
+          .append("tr");
+
+        let tips = {
+          "Zipcode: ": d3.format(".0f")(d),
+          "Neighborhoods: ": zipToNeighborhood[d]
+        };
+
+        rows.append("th").text(key => key).style("padding-right", "10px");
+        rows.append("td").text(key => tips[key]);
+      }).on("mousemove", tooltipMove)
+      .on("mouseout", tooltipLeave);
   }
 
   let drawLegend = series => {
@@ -221,9 +281,21 @@ let timeBarChart = data => {
         'size': 10
       }
     };
+
+    // Drop shadow for more contrast
+    svg.select("g#legend").append("rect")
+      .attr("fill", "rgba(190, 199, 189, 0.5)")
+      .attr("x", lConfig.x + 2)
+      .attr("y", lConfig.y + 2)
+      .attr("rx", 8)
+      .attr("ry", 8)
+      .attr("width", lConfig.width)
+      .attr("height", lConfig.height);
+
     // Legend from: https://www.d3-graph-gallery.com/graph/custom_legend.html
     svg.select("g#legend").append("rect")
-      .attr("fill", "black")
+      .attr("fill", "rgba(30, 30, 30, 1)")
+      .attr("stroke", "rgba(90, 90, 90, 1)")
       .attr("x", lConfig.x)
       .attr("y", lConfig.y)
       .attr("rx", 8)
