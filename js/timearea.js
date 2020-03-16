ac65c090b7e50b30f54454a7e551c61ec9e42d2b
ewwong2 <ewwong2@dons.usfca.edu>
2020-03-15 22:28:24 -0700

    Finalize Visualizations

--- js/timearea.js
+++ js/timearea.js
@@ -1,88 +1,31 @@
-function timeAreaChart(data, update) {
-  // setup svg
+let timeAreaChart = (data, update) => {
+  // Set up chart references
   const svg = d3.select('body').select('svg#timearea');
+  const plot = svg.select('g#areaplot');
 
-  // get current hard-coded size of svg
+  // Set up chart sizes
   const size = {
     width: parseInt(svg.attr('width')),
     height: parseInt(svg.attr('height'))
   };
-
-  // set padding
-  const pad = {top: 15, right: 15, bottom: 25, left: 50};
-
-  // setup plot area
-  const plot = svg.select('g#areaplot');
-
-  // setup plot width and height
+  const pad = {top: 15, right: 15, bottom: 30, left: 50};
   const width = size.width - pad.left - pad.right;
   const height = size.height - pad.top - pad.bottom;
 
+  // Set up axes
   const x = d3.scaleTime()
     .range([pad.left, size.width - pad.right])
-
   const y = d3.scaleLinear()
     .range([height + pad.top, pad.top])
     .nice();
 
-  var formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en")
-
-  function draw(data) {
+  let draw = data => {
     let series = dataWrangling(data);
-    console.log(series);
-    console.log(d3.extent(series, d => new Date(d.key)));
-    x.domain(d3.extent(series, d => new Date(d.key)));
-    y.domain([0, d3.max(series, d => d.value)]);
 
-    xAxis = g => g
-      .attr("transform", `translate(0,${size.height - pad.bottom})`)
-      .call(d3.axisBottom(x).ticks(width / 30).tickSizeOuter(0))
-      .call(g => g.selectAll(".domain"))
-
-    yAxis = g => g
-      .attr("transform", `translate(${pad.left},0)`)
-      .call(d3.axisLeft(y).ticks(width / 350, "s").tickSizeOuter(0))
-      .call(g => g.selectAll(".domain"))
-
-    svg.select('g#areax')
-      .call(xAxis);
-
-    svg.select('g#areay')
-      .call(yAxis);
-
-    let area = d3.area().curve(d3.curveLinear)
-      .x(d => x(new Date(d.key)))
-      .y0(y(0))
-      .y1(d => y(d.value));
-
-    plot.select("g#area").append("path")
-      .datum(series)
-      .attr("fill", "#fecc5c")
-      .attr("d", area);
-
-    let brush = d3.brushX()
-      .extent([[pad.left, pad.top], [size.width - pad.right, size.height - pad.bottom]])
-      .on("end", brushend);
-
-    plot.append("g")
-      .attr("id", "brush")
-      .call(brush);
-
-    function brushend() {
-      let selection = d3.event.selection;
-      if (selection == null) {
-        update(null, true);
-      }
-      if (!d3.event.sourceEvent || !selection) return;
-
-      update(selection.map(d => x.invert(d)), true);
-    }
-
-    // // since our g elements are already in the proper order, we can draw our svg
-    // // elements in any order we want
-    // drawLines(data);
-    // drawAxis(data);
-    // drawLegend(data);
+    drawAxis(series);
+    drawArea(series);
+    drawBrush();
+    return this;
   }
 
   function dataWrangling(data) {
@@ -102,90 +45,78 @@ function timeAreaChart(data, update) {
     return data2;
   }
 
-  /**
-   * Draw the axes for the parallel coordinates
-   */
-  function drawAxis(data) {
-    // // Draw the axes
-    // svg.select('g#y').selectAll('myAxis')
-    //   // For each dimension of the dataset I add a 'g' element:
-    //   .data(dimensions).enter()
-    //   .append('g')
-    //   .attr("class", "axis")
-    //   // translate element to its right position on the x axis
-    //   .attr('transform', d => `translate(${x(d)})`)
-    //   // And I build the axis with the call function
-    //   .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
-    //   // Add axis title
-    //   .append('text')
-    //     .style('text-anchor', 'middle')
-    //     .attr('y', height)
-    //     .attr('dy', 15)
-    //     .text(d => d)
-    //     .style('fill', 'black');
+  let drawAxis = series => {
+    x.domain(d3.extent(series, d => new Date(d.key)));
+    y.domain([0, d3.max(series, d => d.value)]);
+
+    xAxis = g => g
+      .attr("transform", `translate(0,${size.height - pad.bottom})`)
+      .call(d3.axisBottom(x).ticks(width / 30).tickSizeOuter(0))
+      .call(g => g.selectAll(".domain"))
+    yAxis = g => g
+      .attr("transform", `translate(${pad.left},0)`)
+      .call(d3.axisLeft(y).ticks(width / 350, "s").tickSizeOuter(0))
+      .call(g => g.selectAll(".domain"))
+
+    svg.select('g#areax')
+      .call(xAxis);
+    svg.select('g#areay')
+      .call(yAxis);
+
+    svg.select('g#areax').append("text")
+      .attr("fill", "white")
+      .attr("font-weight", "bold")
+      .attr("x", width/2 + pad.left)
+      .attr("y", 30)
+      .attr("text-anchor", "middle")
+      .text("Call Date");
+
+    svg.select('g#areay').append("text")
+      .attr("fill", "white")
+      .attr("font-weight", "bold")
+      .attr("x", 0)
+      .attr("y", pad.top -5)
+      .attr("text-anchor", "middle")
+      .text("Average Minutes");
+    return [x, y];
   }
 
-  function drawLegend(data) {
-    // let types = [... new Set(data.map(d => d['Type']))];
-    // let lConfig = {
-    //   'x': 780,
-    //   'y': 20,
-    //   'width': 175,
-    //   'height': 120,
-    //   'p': 10,
-    //   'marker': {
-    //     'width': 15,
-    //     'height': 3,
-    //     'size': 15
-    //   }
-    // };
-    // // Legend from: https://www.d3-graph-gallery.com/graph/custom_legend.html
-    // svg.select("g#legend").append("rect")
-    //   .attr("fill", "whitesmoke")
-    //   .attr("stroke-width", 1)
-    //   .attr("stroke", "gainsboro")
-    //   .attr("x", lConfig.x)
-    //   .attr("y", lConfig.y)
-    //   .attr("rx", 8)
-    //   .attr("ry", 8)
-    //   .attr("width", lConfig.width)
-    //   .attr("height", lConfig.height);
-    //
-    // svg.select("g#legend").append("text")
-    //   .attr("x", lConfig.x + lConfig.width/2)
-    //   .attr("y", lConfig.y + 2*lConfig.p)
-    //   .attr("text-anchor", "middle")
-    //   .text("College Type");
-    //
-    // svg.select("g#legend").selectAll("mydots")
-    //   .data(types)
-    //   .enter()
-    //   .append("rect")
-    //     .attr("class", d => `legend ${kebabCase(d)}-legend`)
-    //     .attr("x", lConfig.x + lConfig.p)
-    //     .attr("y", (d,i) => lConfig.y + 3.5*lConfig.p + i*(lConfig.marker.size+5)) // 100 is where the first dot appears. 25 is the distance between dots
-    //     .attr("width", lConfig.marker.width)
-    //     .attr("height", lConfig.marker.height)
-    //     .style("fill", d => color(d))
-    //     .style("alignment-baseline", "middle")
-    //     .on("mouseover", d => {highlight({Type: d})})
-    //     .on("mouseleave", d => {doNotHighlight({Type: d})});
+  let drawArea = series => {
+    let area = d3.area().curve(d3.curveLinear)
+      .x(d => x(new Date(d.key)))
+      .y0(y(0))
+      .y1(d => y(d.value));
 
-    // Add one dot in the legend for each name.
-    // svg.select("g#legend").selectAll("mylabels")
-    //   .data(types)
-    //   .enter()
-    //   .append("text")
-    //     .attr("class", d => `legend ${kebabCase(d)}-legend`)
-    //     .attr("x", lConfig.x + 2*lConfig.p + lConfig.marker.size*1.2)
-    //     .attr("y", (d,i) => lConfig.y + 3*lConfig.p  + i*(lConfig.marker.size+5) + (lConfig.marker.size/2)) // 100 is where the first dot appears. 25 is the distance between dots
-    //     .style("fill", d => color(d))
-    //     .text(d => d)
-    //     .attr("text-anchor", "left")
-    //     .style("alignment-baseline", "middle")
-    //     .on("mouseover", d => {highlight({Type: d})})
-    //     .on("mouseleave", d => {doNotHighlight({Type: d})});
+    plot.select("g#area").append("path")
+      .datum(series)
+      .attr("fill", "#fecc5c")
+      .attr("d", area);
+    return area;
+  }
+
+  let drawBrush = () => {
+
+    let brushend = () => {
+      let selection = d3.event.selection;
+      if (selection == null) {
+        update(null, true);
+      }
+      if (!d3.event.sourceEvent || !selection) return;
+
+      update(selection.map(d => x.invert(d)), true);
+      return selection;
+    }
+    let brush = d3.brushX()
+      .extent([[pad.left, pad.top], [size.width - pad.right, size.height - pad.bottom]])
+      .on("end", brushend);
+
+    plot.append("g")
+      .attr("id", "brush")
+      .call(brush);
+    return brush;
   }
 
   draw(data);
+
+  return this;
 }
--- js/timebar.js
+++ js/timebar.js
@@ -1,152 +1,41 @@
-function timeBarChart(data) {
-  // setup svg
+let timeBarChart = data => {
+  // Chart references
   const svg = d3.select('body').select('svg#timebar');
+  const plot = svg.select('g#plot');
 
-  // get current hard-coded size of svg
+  // Sizing
   const size = {
     width: parseInt(svg.attr('width')),
     height: parseInt(svg.attr('height'))
   };
-
-  // set padding
-  const pad = {top: 15, right: 15, bottom: 25, left: 50};
-
-  // setup plot area
-  const plot = svg.select('g#plot');
-
-  // setup plot width and height
+  const pad = {top: 15, right: 15, bottom: 35, left: 50};
   const width = size.width - pad.left - pad.right;
   const height = size.height - pad.top - pad.bottom;
 
+  // Scales
   const x = d3.scaleLinear()
     .range([pad.left, size.width - pad.right])
     .nice()
-
   const y = d3.scaleBand()
     .range([pad.top, size.height - pad.bottom])
     .padding(0.08)
-
   const color = d3.scaleOrdinal()
-    // .range(d3.schemeTableau10);
     .range(d3.schemeYlOrRd[5].reverse());
 
-  var formatValue = x => isNaN(x) ? "N/A" : x.toLocaleString("en");
+  // Check initial chart animation status
+  let animated = true;
 
-  let series;
+  let draw = (data, animate) => {
+    let series = dataWrangling(data);
 
-  function draw(data, animate) {
-    console.log(data);
-    series = dataWrangling(data);
-    console.log(series[0]);
-    x.domain([0, d3.max(series, d => d3.max(d, d => d[1]))]);
-    y.domain(series[0].map(d => d.data.Zipcode));
-
-    color.domain(series.map(d => d.key));
-
-    xAxis = g => g
-      .attr("transform", `translate(0,${size.height - pad.bottom})`)
-      .call(d3.axisBottom(x).ticks(width / 30, "s").tickSizeOuter(0))
-      .call(g => g.selectAll(".domain"))
-
-    yAxis = g => g
-      .attr("transform", `translate(${pad.left},0)`)
-      .call(d3.axisLeft(y).tickSizeOuter(0))
-      .call(g => g.selectAll(".domain"))
-
-    svg.select('g#x')
-      .call(xAxis);
-
-    svg.select('g#y')
-      .call(yAxis);
-
-    console.log(series);
-
-    let bars = plot.selectAll("g#bars")
-      .data(series)
-      .join("g")
-        .attr("fill", d => color(d.key))
-      .selectAll("rect")
-      .data(d => d)
-      .join("rect")
-        .attr("x", d => x(d[0]))
-        .attr("y", (d, i) => y(d.data.Zipcode))
-        .attr("height", y.bandwidth())
-        .attr("width", d => x(d[1]) - x(d[0]))
-        .attr("class", "bar");
-
-    if (animate) {
-        bars.attr("width", 0)
-          .attr("x", d => x(0))
-            .transition()
-            .duration(400)
-            .delay(100)
-          .attr("width", d => x(d[1]) - x(d[0]))
-          .attr("x", d => x(d[0]));
-    }
-
-    plot.selectAll("rect")
-      .on("mouseover", tooltip)
-      .on("mousemove", tooltipMove)
-      .on("mouseout", tooltipLeave);
-
-    function tooltip(d) {
-      let me = d3.select(this);
-      let div = d3.select("body").append("div");
-
-      div.attr("id", "details");
-      div.attr("class", "tooltip");
-
-      let rows = div.append("table")
-        .selectAll("tr")
-        .data(["Time: ", "Time: ", "Time: ", "Time: "])
-        .enter()
-        .append("tr");
-
-      rows.append("th").text(key => key);
-      rows.append("td").text(key => d[key]);
-
-      plot.selectAll("rect").filter(e => (d.key !== e.key)).transition().duration(100).style("fill", "#bbbbbb");
-    }
-
-    function tooltipMove(d) {
-      let div = d3.select("div#details");
-
-      // get height of tooltip
-      let bbox = div.node().getBoundingClientRect();
-
-      // https://stackoverflow.com/questions/4666367/how-do-i-position-a-div-relative-to-the-mouse-pointer-using-jquery
-      div.style("left", d3.event.pageX + "px")
-      div.style("top",  (d3.event.pageY - bbox.height) + "px");
-    }
-
-    function tooltipLeave(d) {
-      d3.selectAll("div#details").remove();
-
-      plot.selectAll("rect").transition().style("fill", d => color(d.key));
-    }
-
-    // Type will be used as color so we will not make an axis for it
-    // dimensions = d3.keys(data[0]).filter(d => d != "Type");
-    //
-    // for (dim in dimensions) {
-    //   let name = dimensions[dim];
-    //   y[name] = d3.scaleLinear()
-    //     .domain(d3.extent(data, d => d[name]))
-    //     .range([height, 0]);
-    // }
-    //
-    // x.domain(dimensions);
-    //
-    // color.domain(data.map(d => d['Type']));
-    //
-    // // since our g elements are already in the proper order, we can draw our svg
-    // // elements in any order we want
-    // drawLines(data);
-    // drawAxis(data);
-    // drawLegend(data);
+    drawAxis(series);
+    drawBars(series, animate);
+    drawLegend(series);
+    addTooltips();
+    return this;
   }
 
-  function dataWrangling(data) {
+  let dataWrangling = (data) => {
     let data2 = d3.nest()
       .key(d => d['Zipcode of Incident'])
       .rollup(function(v) {
@@ -173,106 +62,226 @@ function timeBarChart(data) {
     return stack;
   }
 
-  /**
-   * Draw the axes for the parallel coordinates
-   */
-  function drawAxis(data) {
-    // // Draw the axes
-    // svg.select('g#y').selectAll('myAxis')
-    //   // For each dimension of the dataset I add a 'g' element:
-    //   .data(dimensions).enter()
-    //   .append('g')
-    //   .attr("class", "axis")
-    //   // translate element to its right position on the x axis
-    //   .attr('transform', d => `translate(${x(d)})`)
-    //   // And I build the axis with the call function
-    //   .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
-    //   // Add axis title
-    //   .append('text')
-    //     .style('text-anchor', 'middle')
-    //     .attr('y', height)
-    //     .attr('dy', 15)
-    //     .text(d => d)
-    //     .style('fill', 'black');
+  let drawAxis = (series) => {
+    // Set axes domains
+    x.domain([0, d3.max(series, d => d3.max(d, d => d[1]))]);
+    y.domain(series[0].map(d => d.data.Zipcode));
+    color.domain(series.map(d => d.key));
+
+    // Set up axes
+    xAxis = g => g
+      .attr("transform", `translate(0,${size.height - pad.bottom})`)
+      .call(d3.axisBottom(x).ticks(width / 30, "s").tickSizeOuter(0))
+      .call(g => g.selectAll(".domain"))
+    yAxis = g => g
+      .attr("transform", `translate(${pad.left},0)`)
+      .call(d3.axisLeft(y).tickSizeOuter(0))
+      .call(g => g.selectAll(".domain"))
+
+    // Call axes
+    svg.select('g#x')
+      .call(xAxis);
+    svg.select('g#y')
+      .call(yAxis);
+
+    svg.select('g#x').append("text")
+      .attr("fill", "white")
+      .attr("font-weight", "bold")
+      .attr("x", width/2 + pad.left)
+      .attr("y", 30)
+      .attr("text-anchor", "middle")
+      .text("Average Minutes");
+
+    svg.select('g#y').append("text")
+      .attr("fill", "white")
+      .attr("font-weight", "bold")
+      .attr("x", 0)
+      .attr("y", pad.top -5)
+      .attr("text-anchor", "left")
+      .text("Zipcode");
+    return [x, y];
   }
 
-  function drawLegend(data) {
-    // let types = [... new Set(data.map(d => d['Type']))];
-    // let lConfig = {
-    //   'x': 780,
-    //   'y': 20,
-    //   'width': 175,
-    //   'height': 120,
-    //   'p': 10,
-    //   'marker': {
-    //     'width': 15,
-    //     'height': 3,
-    //     'size': 15
-    //   }
-    // };
-    // // Legend from: https://www.d3-graph-gallery.com/graph/custom_legend.html
-    // svg.select("g#legend").append("rect")
-    //   .attr("fill", "whitesmoke")
-    //   .attr("stroke-width", 1)
-    //   .attr("stroke", "gainsboro")
-    //   .attr("x", lConfig.x)
-    //   .attr("y", lConfig.y)
-    //   .attr("rx", 8)
-    //   .attr("ry", 8)
-    //   .attr("width", lConfig.width)
-    //   .attr("height", lConfig.height);
-    //
-    // svg.select("g#legend").append("text")
-    //   .attr("x", lConfig.x + lConfig.width/2)
-    //   .attr("y", lConfig.y + 2*lConfig.p)
-    //   .attr("text-anchor", "middle")
-    //   .text("College Type");
-    //
-    // svg.select("g#legend").selectAll("mydots")
-    //   .data(types)
-    //   .enter()
-    //   .append("rect")
-    //     .attr("class", d => `legend ${kebabCase(d)}-legend`)
-    //     .attr("x", lConfig.x + lConfig.p)
-    //     .attr("y", (d,i) => lConfig.y + 3.5*lConfig.p + i*(lConfig.marker.size+5)) // 100 is where the first dot appears. 25 is the distance between dots
-    //     .attr("width", lConfig.marker.width)
-    //     .attr("height", lConfig.marker.height)
-    //     .style("fill", d => color(d))
-    //     .style("alignment-baseline", "middle")
-    //     .on("mouseover", d => {highlight({Type: d})})
-    //     .on("mouseleave", d => {doNotHighlight({Type: d})});
+  let drawBars = (series, animate) => {
+    let bars = plot.selectAll("g#bars")
+      .data(series)
+      .join("g")
+        .attr("fill", d => color(d.key))
+      .selectAll("rect")
+      .data(d => d)
+      .join("rect")
+        .attr("x", d => x(d[0]))
+        .attr("y", (d, i) => y(d.data.Zipcode))
+        .attr("height", y.bandwidth())
+        .attr("width", d => x(d[1]) - x(d[0]))
+        .attr("class", "bar");
+
+    animated = true;
+    if (animate) {
+      animated = false;
+      bars.attr("width", 0)
+        .attr("x", d => x(0))
+          .transition()
+          .duration(400)
+          .delay(100)
+        .attr("width", d => x(d[1]) - x(d[0]))
+        .attr("x", d => x(d[0]))
+        .on("end", function(d) { animated=true; });
+    }
+    return bars;
+  }
+
+  let highlight = (key) => {
+    if (!animated) {
+      plot.selectAll("rect").filter(e => (key !== e.key))
+        .transition().duration(100).delay(400)
+        .style("opacity", 0.2);
+    } else {
+      plot.selectAll("rect").filter(e => (key !== e.key))
+        .transition().duration(100)
+        .style("opacity", 0.2);
+    }
+  }
+
+  let unhighlight = () => {
+    if (!animated) {
+      plot.selectAll("rect")
+        .transition().duration(100).delay(400)
+        .style("opacity", 1);
+    } else {
+      plot.selectAll("rect")
+        .transition().duration(100)
+        .style("opacity", 1);
+    }
+  }
+
+  let addTooltips = () => {
+    let tooltip = d => {
+      let me = d3.select(this);
+      let div = d3.select("body").append("div");
+
+      div.attr("id", "details");
+      div.attr("class", "tooltip");
+
+      let rows = div.append("table")
+        .selectAll("tr")
+        .data(["Type: ", "Average: ", "Total Average: ", "Total Count: ", "Zipcode: "])
+        .enter()
+        .append("tr");
+
+      let formatDecimal = d3.format(".2f");
+
+      let tips = {
+        "Type: ": d.key,
+        "Average: ": formatDecimal(d[1] - d[0]) + " minutes",
+        "Total Average: ": formatDecimal(d.data.Total) + " minutes",
+        "Total Count: ": d.data.Count + " calls",
+        "Zipcode: ": d3.format(".0f")(d.data.Zipcode)
+      };
+
+      rows.append("th").text(key => key).style("padding-right", "10px");
+      rows.append("td").text(key => tips[key]);
+
+      highlight(d.key);
+      return rows;
+    }
+    let tooltipMove = d => {
+      let div = d3.select("div#details");
+
+      // get height of tooltip
+      let bbox = div.node().getBoundingClientRect();
+
+      // https://stackoverflow.com/questions/4666367/how-do-i-position-a-div-relative-to-the-mouse-pointer-using-jquery
+      div.style("left", d3.event.pageX + "px")
+      div.style("top",  (d3.event.pageY - bbox.height) + "px");
+      return div;
+    }
+    let tooltipLeave = d => {
+      let me = d3.select(this);
+      d3.selectAll("div#details").remove();
+
+      unhighlight();
+      return me;
+    }
+    plot.selectAll("rect")
+      .on("mouseover", tooltip)
+      .on("mousemove", tooltipMove)
+      .on("mouseout", tooltipLeave);
+  }
+
+  let drawLegend = series => {
+    let types = ['Avg. Received to Entry', 'Avg. Entry to Dispatch', 'Avg. Dispatch to Response', 'Avg. Response to On Scene'];
+    let lConfig = {
+      'x': 700,
+      'y': 350,
+      'width': 225,
+      'height': 100,
+      'p': 10,
+      'marker': {
+        'size': 10
+      }
+    };
+    // Legend from: https://www.d3-graph-gallery.com/graph/custom_legend.html
+    svg.select("g#legend").append("rect")
+      .attr("fill", "black")
+      .attr("x", lConfig.x)
+      .attr("y", lConfig.y)
+      .attr("rx", 8)
+      .attr("ry", 8)
+      .attr("width", lConfig.width)
+      .attr("height", lConfig.height);
+
+    svg.select("g#legend").append("text")
+      .attr("fill", "white")
+      .attr("font-weight", "bold")
+      .attr("x", lConfig.x + lConfig.width/2)
+      .attr("y", lConfig.y + 2*lConfig.p)
+      .attr("text-anchor", "middle")
+      .text("Time Stages");
+
+    svg.select("g#legend").selectAll("mydots")
+      .data(types)
+      .enter()
+      .append("rect")
+        .attr("x", lConfig.x + lConfig.p)
+        .attr("y", (d,i) => lConfig.y + 3*lConfig.p + i*(lConfig.marker.size+5))
+        .attr("width", lConfig.marker.size)
+        .attr("height", lConfig.marker.size)
+        .style("fill", d => color(d))
+        .style("alignment-baseline", "middle")
+        .on("mouseover", d => highlight(d))
+        .on("mouseleave", d => unhighlight({Type: d}));
 
     // Add one dot in the legend for each name.
-    // svg.select("g#legend").selectAll("mylabels")
-    //   .data(types)
-    //   .enter()
-    //   .append("text")
-    //     .attr("class", d => `legend ${kebabCase(d)}-legend`)
-    //     .attr("x", lConfig.x + 2*lConfig.p + lConfig.marker.size*1.2)
-    //     .attr("y", (d,i) => lConfig.y + 3*lConfig.p  + i*(lConfig.marker.size+5) + (lConfig.marker.size/2)) // 100 is where the first dot appears. 25 is the distance between dots
-    //     .style("fill", d => color(d))
-    //     .text(d => d)
-    //     .attr("text-anchor", "left")
-    //     .style("alignment-baseline", "middle")
-    //     .on("mouseover", d => {highlight({Type: d})})
-    //     .on("mouseleave", d => {doNotHighlight({Type: d})});
+    svg.select("g#legend").selectAll("mylabels")
+      .data(types)
+      .enter()
+      .append("text")
+        .attr("x", lConfig.x + 2*lConfig.p + lConfig.marker.size*1.2)
+        .attr("y", (d,i) => lConfig.y + 3*lConfig.p  + i*(lConfig.marker.size+5) + (lConfig.marker.size/2))
+        .style("fill", d => color(d))
+        .text(d => d)
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+        .on("mouseover", d => highlight(d))
+        .on("mouseleave", d => unhighlight({Type: d}));
+    return svg.select("g#legend");
   }
 
-  function updateSelection(selection, animate) {
+  let updateSelection = (selection, animate) => {
     if (selection == null || selection.length == 0 || selection[0].getTime() === selection[1].getTime()) {
       var newData = data;
     } else {
       var newData = data.filter(function(d) {
-        // console.log(new Date(d["Call Date"]), new Date(selection[0]), new Date(d["Call Date"]), new Date(selection[1]));
         return new Date(d["Call Date"]) >= new Date(selection[0])
           && new Date(d["Call Date"]) <= new Date(selection[1]);
       });
     }
-    console.log(newData);
     plot.selectAll("rect.bar").remove();
-    draw(newData, animate);
+    return draw(newData, animate);
   }
 
+  // Initial first draw
   draw(data, true);
 
   return updateSelection;
